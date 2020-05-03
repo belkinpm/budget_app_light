6800fe4bb6308b41781598a5f12c424673973544
Pavel Belkin <belkinpm@gmail.com>
2020-05-03 15:22:55 +0300

    usable frontend

--- src/main.js
+++ src/main.js
@@ -43,18 +43,21 @@ class Input {
     <div class="input-group fadeIn" id="${this.name}">
       <div class="input-text-group">
         <label>${this.name}:</label>
-        <input class="expence input" type="text" placeholder="Input value" onblur="checkNum()" />
+        <input class="expence input" type="text" placeholder="Input value" onblur="dataCollector()" />
         <button type="button" class="btn btn-delete" onclick="deleteInput(${this.name})">&times;</button>
       </div>
-      <input type="checkbox" class="check">
+      <div class="switch-wrap">
+        <input type="checkbox" class="check" onblur="weekChange()"/>
+        <div class="switch"></div>
+      </div>
     </div>
   `;
   }
 }
 
+// === Render Elements ===
 const expencesList = new List();
 
-// === All Functions ===
 function createInput(target) {
   const input = new Input(target.id);
   document
@@ -69,53 +72,89 @@ function deleteInput(target) {
     .querySelector('.buttons-group')
     .insertAdjacentHTML('beforeend', expenceBtn.renderBtn());
   target.remove();
+  delete inputData[target.id];
 }
 
-function checkNum() {
+// === Data Collection ===
+const inputData = {};
+
+function dataCollector() {
+  let parent = event.target.parentElement;
+  let label = parent.querySelector('label');
+  let checkbox = parent.parentElement.querySelector('.check');
+
   if (isNaN(event.target.value)) {
     event.target.classList.add('alert');
-    event.target.previousElementSibling.classList.add('alert');
+    if (label) {
+      label.classList.add('alert');
+    }
   } else {
     event.target.classList.remove('alert');
-    event.target.previousElementSibling.classList.remove('alert');
+    if (label) {
+      label.classList.remove('alert');
+      inputData[parent.parentElement.id] = {
+        value: +event.target.value,
+        week: checkbox.checked,
+      };
+    } else {
+      inputData[event.target.id] = +event.target.value;
+    }
   }
 }
 
-function countBudget() {
-  const input = document.querySelectorAll('.input');
-  const checkbox = document.querySelectorAll('.check');
-
-  let budget = +document.querySelector('#income').value;
-  let result = document.querySelector('.result');
-
-  result.innerHTML = '';
-
-  function getAdvice(budget, prop) {
-    const advice = {
-      error: `<p>Please, check red fields and enter only numbers or leave a blank fields</p>`,
-      good: `<p>Congradulations!\n
-            You can save ${budget} Rubles during next two weeks!</p>`,
-      bad: `<p>You need to find ${budget} Rubles for next two weeks!\n
-            Please, cut your expences or find additional income!`,
-      tight: `<p>Your budget is very tight!\n
-              You need to check your expences everyday!</p>`,
+function weekChange() {
+  let parent = event.target.parentElement.parentElement;
+  if (inputData[parent.id] !== undefined) {
+    inputData[parent.id].week = !inputData[parent.id].week;
+  } else {
+    inputData[parent.id] = {
+      value: 0,
+      week: event.target.checked,
     };
-    result.insertAdjacentHTML('beforeend', advice[prop]);
   }
+}
 
-  for (let i = 1; i < input.length; i++) {
-    if (checkbox[i - 1].checked) {
-      budget -= input[i].value;
-    } else {
-      budget -= input[i].value * 2;
+// === Count & Clear ===
+function countBudget() {
+  let result = document.querySelector('.result');
+  let budget = inputData.income;
+  result.innerHTML = '';
+  
+  for (let key in inputData) {
+    if (key != 'income') {
+      if (inputData[key].week) {
+        budget -= inputData[key].value;
+      } else {
+        budget -= inputData[key].value * 2;
+      }
     }
   }
-  if (isNaN(budget)) {
+
+  function getAdvice(budget, property) {
+    const advice = {
+      error: `<p>Please, check red fields and enter only numbers or leave a blank fields.</p>`,
+      empty: `<p>Income field is empty!<br>
+            Please, fill in at least this field.</p>`,
+      good: `<p>Congradulations!<br>
+            You can save ${budget} Rubles during next two weeks!</p>`,
+      bad: `<p>You need to find ${budget} Rubles for next two weeks!<br>
+            Please, cut your expences or find additional income!`,
+      tight: `<p>Your budget is very tight!<br>
+              You need to check your expences everyday!</p>`,
+    };
+    result.insertAdjacentHTML('beforeend', advice[property]);
+  }
+
+  location.href = '#result';
+
+  if (isNaN(budget) && budget) {
     getAdvice(budget, 'error');
+  } else if (!budget && budget !== 0) {
+    getAdvice(budget, 'empty');
   } else if (budget > 0) {
     getAdvice(budget, 'good');
   } else if (budget < 0) {
-    budget = budget * -1;
+    budget = -budget;
     getAdvice(budget, 'bad');
   } else {
     getAdvice(budget, 'tight');
@@ -123,16 +162,15 @@ function countBudget() {
 }
 
 function clearAll() {
-  const input = document.querySelectorAll('.input');
-  const checkbox = document.querySelectorAll('.check');
+  const inputs = document.querySelectorAll('.input');
+  document.querySelector('.result').innerHTML = '';
 
-  let result = document.querySelector('.result');
-
-  for (let i = 0; i < input.length; i++) {
-    input[i].value = '';
-    if (checkbox[i] != undefined) checkbox[i].checked = false;
-    input[i].classList.remove('alert');
-    input[i].previousElementSibling.classList.remove('alert');
+  for (let input of inputs) {
+    input.classList.remove('alert');
+    input.previousElementSibling.classList.remove('alert');
   }
-  result.innerHTML = '';
+
+  for (let key in inputData) {
+    delete inputData[key];
+  }
 }
